6b3d3f564912854103170a20901fadb4d42992ad
KOKUMUbooker <bookerochieng20@gmail.com>
2026-03-01 10:24:22 +0300

    Set up controller for MovieNight & its action methods

---
+++ Controllers/MovieNightController.cs
@@ -0,0 +1,121 @@
+namespace WatchHive.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using WatchHive.Models;
+using WatchHive.DTOs;
+using Microsoft.EntityFrameworkCore;
+
+[ApiController]
+[Route("/api")]
+public class MovieNightController : ControllerBase
+{
+    private readonly WatchHiveDbContext _dbContext;
+
+    public MovieNightController(WatchHiveDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    [HttpPost("/groups/{groupId}/movie-night")]
+    public async Task<IActionResult> CreateMovieNight([FromRoute] string groupId, [FromBody] CreateMovieNightDto movieNightDto)
+    {
+        if (!Guid.TryParse(groupId, out Guid parsedGroupId))
+        {
+            return BadRequest(new CustomError { Message = "Invalid groupId provided" });
+        }
+
+        if (!Guid.TryParse(movieNightDto.CreatedById, out Guid parsedUserId))
+        {
+            return BadRequest(new CustomError { Message = "Invalid groupId provided" });
+        }
+
+        var groupExists = await _dbContext.Groups.FindAsync(parsedGroupId);
+        if (groupExists == null)
+        {
+            return NotFound(new CustomError{ Message = "The movie night's group does not exist" });
+        }
+
+        var userExist = await _dbContext.Users.FindAsync(parsedUserId);
+        if (userExist == null)
+        {
+            return NotFound(new CustomError{ Message = "The user creating the movie night does not exist" });
+        }
+
+        var movieNight = new MovieNightEvent
+        {
+            CreatedById = parsedUserId,
+            GroupId = parsedGroupId,
+            ScheduledAt = movieNightDto.ScheduledAt
+        };
+
+        await _dbContext.MovieNightEvents.AddAsync(movieNight);
+
+        return Ok(new { Message = "Movie night event created successfully" });
+    }
+
+    [HttpGet("/groups/{groupId}/movie-nights")]
+    public async Task<IActionResult> FetchMovieNights([FromRoute] string groupId)
+    {
+        if (!Guid.TryParse(groupId, out Guid parsedGroupId))
+        {
+            return BadRequest(new CustomError { Message = "Invalid groupId provided" });
+        }
+
+        var groupExists = await _dbContext.Groups.FindAsync(parsedGroupId);
+        if (groupExists == null)
+        {
+            return NotFound(new CustomError{ Message = "The movie night's group does not exist" });
+        }
+
+        var MovieNights = await _dbContext.MovieNightEvents
+             .Where(mn => mn.GroupId == parsedGroupId)
+             .AsNoTracking()
+             .ToListAsync();
+
+        return Ok(new { MovieNights });
+    }
+
+    [HttpGet("/movie-nights/{movieNightId}")]
+    public async Task<IActionResult> FetchMovieNight([FromRoute] string movieNightId)
+    {
+        if (!Guid.TryParse(movieNightId, out Guid parsedMovieNightId))
+        {
+            return BadRequest(new CustomError { Message = "Invalid movieNightId provided" });
+        }
+        
+        var MovieNight = await _dbContext.MovieNightEvents.FindAsync(parsedMovieNightId);
+
+        if (MovieNight == null)
+        {
+            return NotFound(new CustomError { Message = "Movie night event not found" } );
+        }
+
+        return Ok(new { MovieNight });
+    }
+
+    [HttpPatch("/movie-nights/{movieNightId}")]
+    public async Task<IActionResult> UpdateMovieNight([FromRoute] string movieNightId, [FromBody] UpdateMovieNightDto updateDto)
+    {
+        if (!Guid.TryParse(movieNightId, out Guid parsedMovieNightId))
+        {
+            return BadRequest(new CustomError { Message = "Invalid movieNightId provided" });
+        }
+        
+        var MovieNight = await _dbContext.MovieNightEvents
+                            .Where(mn => mn.Id == parsedMovieNightId)
+                            .FirstAsync();
+
+        if (MovieNight == null)
+        {
+            return NotFound(new CustomError { Message = "Movie night event not found" } );
+        }
+
+        MovieNight.IsLocked = updateDto.IsLocked ?? MovieNight.IsLocked;
+        MovieNight.ScheduledAt = updateDto.ScheduledAt ?? MovieNight.ScheduledAt;
+        MovieNight.SelectedMovieTmdbId = updateDto.SelectedMovieTmdbId;
+
+        await _dbContext.SaveChangesAsync();
+
+        return Ok(new {Message = "Movie night updated successfully" });
+    }    
+}
---
+++ DTOs/MovieNightDtos.cs
@@ -0,0 +1,19 @@
+namespace WatchHive.DTOs;
+
+using System.ComponentModel.DataAnnotations;
+
+public class CreateMovieNightDto
+{
+    [Required]
+    public DateTime ScheduledAt { get; set; }
+
+    [Required]
+    public string CreatedById {get; set;} = null!;
+}
+
+public class UpdateMovieNightDto
+{
+    public DateTime? ScheduledAt { get; set; }
+    public bool? IsLocked { get; set; } = false;
+    public int? SelectedMovieTmdbId { get; set; }
+}
