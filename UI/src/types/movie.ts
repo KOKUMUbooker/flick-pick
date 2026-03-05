ee805704af4274c4bd82bdf0e0231abf0293e306
KOKUMUbooker <bookerochieng20@gmail.com>
2026-03-05 15:46:38 +0300

    Add MovieNight & MovieSuggestion types that are accurate to db types

--- UI/src/types/movie.ts
+++ UI/src/types/movie.ts
@@ -24,3 +24,44 @@ export interface Movie {
     CreatedAt: Date;
     Verified: boolean
 }
+
+// Accurate DB versions
+export interface MovieNightEvent {
+    groupId: string,
+    scheduledAt: string,
+    createdById: string,
+    isLocked: boolean,
+    selectedMovieTmdbId?: number,
+    movieSuggestions: MovieSuggestion[]
+}
+
+export interface MovieSuggestion {
+    id: string,
+    tmdbId: number,
+    movieDetails: TMDBMovieDetails,
+    suggestedBy: { fullName: string, email: string },
+    votes: Vote[],
+    isDisqualified: boolean,
+}
+
+export interface TMDBMovieDetails {
+    tmdbId: number,
+    title: string,
+    overview: string,
+    posterPath: string,
+    runtime: number,
+    releaseDate: string
+}
+
+export interface Vote {
+    userId: string;
+    movieSuggestionId: string;
+    voteType: VoteType
+    user: { fullName: string, email: string }
+}
+
+export enum VoteType {
+    Upvote = 1,
+    Downvote = 2,
+    Veto = 3
+}
