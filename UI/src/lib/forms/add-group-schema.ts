c87ebba86c598a85b662fa7c00b54891a09f6c84
KOKUMUbooker <bookerochieng20@gmail.com>
2026-03-07 10:25:55 +0300

    Set up schema for adding a group

---
+++ UI/src/lib/forms/add-group-schema.ts
@@ -0,0 +1,12 @@
+import z from "zod";
+
+export const addGroupSchema = z.object({
+    Name: z
+        .string()
+        .min(1, 'Name is required')
+        .max(64, 'Name must be less than 64 characters')
+        .trim(),
+    Description: z
+        .string()
+        .optional()
+})
