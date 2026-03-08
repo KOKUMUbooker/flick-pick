8bb94dfff0def58e11968530ffaffd29d7911d05
KOKUMUbooker <bookerochieng20@gmail.com>
2026-03-08 17:17:02 +0300

    Switch to using DateTimeOffset instead of DateTime so as to preserver user's timezone

--- DTOs/UserGroupDto.cs
+++ DTOs/UserGroupDto.cs
@@ -6,6 +6,6 @@ public class UserGroupDto
     public string UserId { get; set; } = null!;
     public string FullName { get; set; } = null!;
     public string Email { get; set; } = null!;
-    public DateTime JoinedAt { get; set; }
+    public DateTimeOffset JoinedAt { get; set; }
     public bool IsAdmin { get; set; }
 }
