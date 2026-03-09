c3684fb14640f449e39c7990f111d67665516ea6
KOKUMUbooker <bookerochieng20@gmail.com>
2026-03-09 18:43:28 +0300

    Add mock TMDB result

--- UI/src/data/mock-tmdb-res.ts
+++ UI/src/data/mock-tmdb-res.ts
@@ -3,132 +3,132 @@ import type { TmdbMovieResult, TmdbSearchResponse } from "../api/types";
 
 export const dummyMovieResults: TmdbMovieResult[] = [
     {
-        id: 550,
+        tmdbId: 550,
         title: "Fight Club",
-        poster_path: "/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg",
-        release_date: "1999-10-15",
+        posterPath: "/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg",
+        releaseDate: "1999-10-15",
         overview: "A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground 'fight clubs' forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.",
-        vote_average: 8.4
+        voteAverage: 8.4
     },
     {
-        id: 680,
+        tmdbId: 680,
         title: "Pulp Fiction",
-        poster_path: "/d5iIlFn5s0ImszYzBPb8JPIfbXD.jpg",
-        release_date: "1994-09-10",
+        posterPath: "/d5iIlFn5s0ImszYzBPb8JPIfbXD.jpg",
+        releaseDate: "1994-09-10",
         overview: "A burger-loving hit man, his philosophical partner, a drug-addled gangster's moll and a washed-up boxer converge in this sprawling, comedic crime caper. Their adventures unfurl in three stories that ingeniously trip back and forth in time.",
-        vote_average: 8.5
+        voteAverage: 8.5
     },
     {
-        id: 27205,
+        tmdbId: 27205,
         title: "Inception",
-        poster_path: "/9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg",
-        release_date: "2010-07-16",
+        posterPath: "/9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg",
+        releaseDate: "2010-07-16",
         overview: "Cobb, a skilled thief who commits corporate espionage by infiltrating the subconscious of his targets, is offered a chance to regain his old life as payment for a task considered to be impossible: 'inception', the implantation of another person's idea into a target's subconscious.",
-        vote_average: 8.4
+        voteAverage: 8.4
     },
     {
-        id: 155,
+        tmdbId: 155,
         title: "The Dark Knight",
-        poster_path: "/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
-        release_date: "2008-07-18",
+        posterPath: "/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
+        releaseDate: "2008-07-18",
         overview: "Batman raises the stakes in his war on crime. With the help of Lt. Jim Gordon and District Attorney Harvey Dent, Batman sets out to dismantle the remaining criminal organizations that plague the streets. The partnership proves to be effective, but they soon find themselves prey to a reign of chaos unleashed by a rising criminal mastermind known to the terrified citizens of Gotham as the Joker.",
-        vote_average: 8.5
+        voteAverage: 8.5
     },
     {
-        id: 278,
+        tmdbId: 278,
         title: "The Shawshank Redemption",
-        poster_path: "/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
-        release_date: "1994-09-23",
+        posterPath: "/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg",
+        releaseDate: "1994-09-23",
         overview: "Framed in the 1940s for the double murder of his wife and her lover, upstanding banker Andy Dufresne begins a new life at the Shawshank prison, where he puts his accounting skills to work for an amoral warden. During his long stretch in prison, Dufresne comes to be admired by the other inmates -- including an older prisoner named Red -- for his integrity and unquenchable sense of hope.",
-        vote_average: 8.7
+        voteAverage: 8.7
     },
     {
-        id: 238,
+        tmdbId: 238,
         title: "The Godfather",
-        poster_path: "/3bhkrj58Vtu7enYsRolD1fZdja1.jpg",
-        release_date: "1972-03-14",
+        posterPath: "/3bhkrj58Vtu7enYsRolD1fZdja1.jpg",
+        releaseDate: "1972-03-14",
         overview: "Spanning the years 1945 to 1955, a chronicle of the fictional Italian-American Corleone crime family. When organized crime family patriarch Vito Corleone barely survives an attempt on his life, his youngest son, Michael, steps in to take care of the would-be killers, launching a campaign of bloody revenge.",
-        vote_average: 8.7
+        voteAverage: 8.7
     },
     {
-        id: 424,
+        tmdbId: 424,
         title: "Schindler's List",
-        poster_path: "/sF1U4EUQS8YHUYjNl3pMGNIQyr0.jpg",
-        release_date: "1993-12-15",
+        posterPath: "/sF1U4EUQS8YHUYjNl3pMGNIQyr0.jpg",
+        releaseDate: "1993-12-15",
         overview: "The true story of how businessman Oskar Schindler saved over a thousand Jewish lives from the Nazis while they worked as slaves in his factory during World War II.",
-        vote_average: 8.6
+        voteAverage: 8.6
     },
     {
-        id: 129,
+        tmdbId: 129,
         title: "Spirited Away",
-        poster_path: "/39wmItIWsg5sZMyRUHLkWBcuVCM.jpg",
-        release_date: "2001-07-20",
+        posterPath: "/39wmItIWsg5sZMyRUHLkWBcuVCM.jpg",
+        releaseDate: "2001-07-20",
         overview: "A young girl, Chihiro, becomes trapped in a strange new world of spirits. When her parents undergo a mysterious transformation, she must call upon the courage she never knew she had to free her family.",
-        vote_average: 8.6
+        voteAverage: 8.6
     },
     {
-        id: 19404,
+        tmdbId: 19404,
         title: "Dilwale Dulhania Le Jayenge",
-        poster_path: "/uC6TTUhPpQCmgldGyYveKRAu8JN.jpg",
-        release_date: "1995-10-20",
+        posterPath: "/uC6TTUhPpQCmgldGyYveKRAu8JN.jpg",
+        releaseDate: "1995-10-20",
         overview: "Raj is a rich, carefree, happy-go-lucky second generation NRI. Simran is the daughter of Chaudhary Baldev Singh, who in spite of being an NRI is very strict about adherence to Indian values. Simran has left for India to be married to her childhood fiancé. Raj leaves for India with a mission at his hands, to claim his lady love under the noses of her whole family. Thus begins a saga.",
-        vote_average: 8.6
+        voteAverage: 8.6
     },
     {
-        id: 13,
+        tmdbId: 13,
         title: "Forrest Gump",
-        poster_path: "/arw2vcBveWOVZr6pxd9XTd1TdQa.jpg",
-        release_date: "1994-07-06",
+        posterPath: "/arw2vcBveWOVZr6pxd9XTd1TdQa.jpg",
+        releaseDate: "1994-07-06",
         overview: "A man with a low IQ has accomplished great things in his life and been present during significant historic events—in each case, far exceeding what anyone imagined he could do. But despite all he has achieved, his one true love eludes him.",
-        vote_average: 8.5
+        voteAverage: 8.5
     },
     {
-        id: 769,
+        tmdbId: 769,
         title: "GoodFellas",
-        poster_path: "/aKuFiU82s5ISJpGZp7YkIr3kCUd.jpg",
-        release_date: "1990-09-12",
+        posterPath: "/aKuFiU82s5ISJpGZp7YkIr3kCUd.jpg",
+        releaseDate: "1990-09-12",
         overview: "The true story of Henry Hill, a half-Irish, half-Sicilian Brooklyn kid who is adopted by neighbourhood gangsters at an early age and climbs the ranks of a Mafia family under the guidance of Jimmy Conway.",
-        vote_average: 8.5
+        voteAverage: 8.5
     },
     {
-        id: 12477,
+        tmdbId: 12477,
         title: "Grave of the Fireflies",
-        poster_path: "/k9tv1rXZbOhH7eiCk378x61kNQ1.jpg",
-        release_date: "1988-04-16",
+        posterPath: "/k9tv1rXZbOhH7eiCk378x61kNQ1.jpg",
+        releaseDate: "1988-04-16",
         overview: "In the final months of World War II, 14-year-old Seita and his four-year-old sister Setsuko are orphaned when their mother is killed during an air raid in Kobe, Japan. After a falling out with their aunt, they move into an abandoned bomb shelter. With no surviving relatives and their emergency rations depleted, Seita and Setsuko struggle to survive.",
-        vote_average: 8.5
+        voteAverage: 8.5
     },
     {
-        id: 11216,
+        tmdbId: 11216,
         title: "Cinema Paradiso",
-        poster_path: "/8SRUfRUi6x4O68n0VC8D6aAoquw.jpg",
-        release_date: "1988-11-17",
+        posterPath: "/8SRUfRUi6x4O68n0VC8D6aAoquw.jpg",
+        releaseDate: "1988-11-17",
         overview: "A filmmaker recalls his childhood, when he fell in love with the movies at his village's theater and formed a deep friendship with the theater's projectionist.",
-        vote_average: 8.5
+        voteAverage: 8.5
     },
     {
-        id: 637,
+        tmdbId: 637,
         title: "Life Is Beautiful",
-        poster_path: "/f7DImXDebOs148U4uPjI61iDvaK.jpg",
-        release_date: "1997-12-20",
+        posterPath: "/f7DImXDebOs148U4uPjI61iDvaK.jpg",
+        releaseDate: "1997-12-20",
         overview: "A touching story of an Italian book seller of Jewish ancestry who lives in his own little fairy tale. His creative and happy life would come to an abrupt halt when his entire family is deported to a concentration camp during World War II. While locked up he tries to convince his son that the whole thing is just a game.",
-        vote_average: 8.5
+        voteAverage: 8.5
     },
     {
-        id: 311,
+        tmdbId: 311,
         title: "Once Upon a Time in America",
-        poster_path: "/i0Bh0E6XoM6ezGyiBmGPBHLDYhT.jpg",
-        release_date: "1984-05-23",
+        posterPath: "/i0Bh0E6XoM6ezGyiBmGPBHLDYhT.jpg",
+        releaseDate: "1984-05-23",
         overview: "A former Prohibition-era Jewish gangster returns to the Lower East Side of Manhattan over thirty years later, where he once again must confront the ghosts and regrets of his old life.",
-        vote_average: 8.5
+        voteAverage: 8.5
     }
 ];
 
 export const dummySearchResponse: TmdbSearchResponse = {
     page: 1,
     results: dummyMovieResults,
-    total_pages: 1,
-    total_results: dummyMovieResults.length
+    totalPages: 1,
+    totalResults: dummyMovieResults.length
 };
 
 // Different search queries for more realistic testing
@@ -144,17 +144,17 @@ export function getDummySearchResponse(query: string): TmdbSearchResponse {
     return {
         page: 1,
         results: filteredResults.length > 0 ? filteredResults : dummyMovieResults.slice(0, 3), // Fallback to first 3 if no matches
-        total_pages: 1,
-        total_results: filteredResults.length || 3
+        totalPages: 1,
+        totalResults: filteredResults.length || 3
     };
 }
 
 // Individual movie for confirmation screen
 export const dummySelectedMovie: TmdbMovieResult = {
-    id: 550,
+    tmdbId: 550,
     title: "Fight Club",
-    poster_path: "/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg",
-    release_date: "1999-10-15",
+    posterPath: "/pB8BM7pdSp6B6Ih7QZ4DrQ3PmJK.jpg",
+    releaseDate: "1999-10-15",
     overview: "A ticking-time-bomb insomniac and a slippery soap salesman channel primal male aggression into a shocking new form of therapy. Their concept catches on, with underground 'fight clubs' forming in every town, until an eccentric gets in the way and ignites an out-of-control spiral toward oblivion.",
-    vote_average: 8.4
+    voteAverage: 8.4
 };
