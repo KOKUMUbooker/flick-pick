f6b075c174b925689bfb621ccdac36ead9461955
KOKUMUbooker <bookerochieng20@gmail.com>
2026-03-21 08:39:09 +0300

    Create mobile only header component that is rendered on mobile screens when tabs get hidden

---
+++ UI/src/lib/components/groups/group-actions-mobile.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import type { DBGroup } from "../../../types";
+    import { EditIcon, MailPlus, Plus, Trash, Users } from '@lucide/svelte';
+	import Button from '../ui/button/button.svelte';
+
+    interface GroupActionsMobile {
+		selectedGroup: DBGroup | null;
+		createNewEvent: () => void;
+ 		showJoinGroupsDialog: boolean;
+		showSendInviteDialog: boolean;
+	}
+	let {
+		createNewEvent,
+ 		selectedGroup,
+		showJoinGroupsDialog = $bindable(),
+		showSendInviteDialog = $bindable()
+	}: GroupActionsMobile = $props();
+</script>
+
+<div class="flex h-16 items-center justify-between">
+    <div class="flex flex-row gap-2 items-center">
+        <Button variant="outline"> <EditIcon/> </Button>
+        <Button variant="destructive"> <Trash/> </Button>
+    </div>
+
+    <div class="flex items-center gap-2">
+        <Button size="sm" variant="outline" onclick={()=>showJoinGroupsDialog = true}>
+            <MailPlus class="mr-2 h-4 w-4" />
+            Join Request
+        </Button>
+        {#if selectedGroup?.isUserAdmin}
+            <Button size="sm" variant="outline" onclick={()=>showSendInviteDialog = true}>
+                <Users class="mr-2 h-4 w-4" />
+                Send invite
+            </Button>
+            <Button size="sm" onclick={createNewEvent}>
+                <Plus class="mr-2 h-4 w-4" />
+                New Event
+            </Button>
+        {/if}
+    </div>
+</div>
