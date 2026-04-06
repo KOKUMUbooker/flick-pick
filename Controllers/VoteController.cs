0c67fed076fe108754a0827b99a1ee4160086e43
KOKUMUbooker <bookerochieng20@gmail.com>
2026-04-06 15:44:04 +0300

    Properly update UpvoteCount & DownvoteCount fields of movie suggestion when voting for a suggestion & aslo send event to client wit updated suggestion

--- Controllers/VoteController.cs
+++ Controllers/VoteController.cs
@@ -1,8 +1,10 @@
 namespace WatchHive.Controllers;
 
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
 using WatchHive.DTOs;
+using WatchHive.Hubs;
 using WatchHive.Models;
 
 [ApiController]
@@ -10,10 +12,12 @@ using WatchHive.Models;
 public class VoteController : ControllerBase
 {
     private readonly WatchHiveDbContext _dbContext;
+    private readonly IHubContext<MovieNightHub> _hubContext;
 
-    public VoteController(WatchHiveDbContext dbContext)
+    public VoteController(WatchHiveDbContext dbContext, IHubContext<MovieNightHub> hubContext)
     {
         _dbContext = dbContext;
+        _hubContext = hubContext;
     }
 
     [HttpGet("movie-suggestions/vote/{voteId}")]
@@ -74,8 +78,12 @@ public class VoteController : ControllerBase
             return BadRequest(new CustomError { Message = "Invalid movieSuggestionId provided" });
         }
 
-        var movieSuggestionExists = await _dbContext.MovieSuggestions.FindAsync(parsedMovieSuggestionId);
-        if (movieSuggestionExists == null)
+        var movieSuggestion = await _dbContext.MovieSuggestions
+            .Include(ms => ms.SuggestedBy)
+            .Include(ms => ms.Movie)
+            .Include(ms => ms.Votes)
+            .FirstOrDefaultAsync(ms => ms.Id == parsedMovieSuggestionId);
+        if (movieSuggestion == null)
         {
             return BadRequest(new CustomError { Message = "Movie suggestion to voted on does not exist" });
         }
@@ -87,7 +95,7 @@ public class VoteController : ControllerBase
 
         // Check if Event has been locked
         bool eventLocked = await _dbContext.MovieNightEvents
-                            .AnyAsync(me => me.Id == movieSuggestionExists.MovieNightEventId && me.IsLocked);
+                            .AnyAsync(me => me.Id == movieSuggestion.MovieNightEventId && me.IsLocked);
 
         if (eventLocked)
         {
@@ -107,17 +115,33 @@ public class VoteController : ControllerBase
                 .FirstOrDefaultAsync();
        
         // If vote was found & its the same vote type, just remove it
+        string movieNightId = movieSuggestion.MovieNightEventId.ToString();
         if (initialVote != null && initialVote.VoteType == createDto.VoteType)
         {
             await _dbContext.Votes 
                     .Where(v => v.UserId == parsedUserId && v.MovieSuggestionId == parsedMovieSuggestionId)
                     .ExecuteDeleteAsync();
-            
-            if (createDto.VoteType == VoteType.Veto && movieSuggestionExists.IsDisqualified)
+ 
+            // Update movie suggestion Upvote & Downvote counts
+            if (initialVote.VoteType == VoteType.Upvote && movieSuggestion.UpvoteCount > 0)
             {
-                movieSuggestionExists.IsDisqualified = false;
-                await _dbContext.SaveChangesAsync();
+                movieSuggestion.UpvoteCount--;
             }
+            else if (initialVote.VoteType == VoteType.Downvote && movieSuggestion.DownVoteCount > 0)
+            {
+                movieSuggestion.DownVoteCount--;
+            }
+            
+            if (createDto.VoteType == VoteType.Veto && movieSuggestion.IsDisqualified)
+            {
+                movieSuggestion.IsDisqualified = false;
+            }
+
+            await _hubContext.Clients
+                .GroupExcept(movieNightId, new[] {createDto.ConnectionId} )
+                .SendAsync("suggestion", movieNightId, ToMovieSuggestionDto(movieSuggestion,parsedUserId), "vote");
+
+            await _dbContext.SaveChangesAsync();
 
             return Ok(new { Message = "Vote removed successfully" });
         }
@@ -125,6 +149,16 @@ public class VoteController : ControllerBase
         // If voteType is different, delete the previous one before adding a new one
         if (initialVote != null && initialVote.VoteType != createDto.VoteType)
         {
+             // Update movie suggestion Upvote & Downvote counts
+            if (initialVote.VoteType == VoteType.Upvote && movieSuggestion.UpvoteCount > 0)
+            {
+                movieSuggestion.UpvoteCount--;
+            }
+            if (initialVote.VoteType == VoteType.Downvote && movieSuggestion.DownVoteCount > 0)
+            {
+                movieSuggestion.DownVoteCount--;
+            }
+            
             await _dbContext.Votes 
                     .Where(v => v.UserId == parsedUserId && v.MovieSuggestionId == parsedMovieSuggestionId)
                     .ExecuteDeleteAsync();
@@ -137,19 +171,60 @@ public class VoteController : ControllerBase
             VoteType = createDto.VoteType
         };
 
+         // Update movie suggestion Upvote & Downvote counts
+        if (createDto.VoteType == VoteType.Upvote)
+        {
+            movieSuggestion.UpvoteCount++;
+        }
+        if (createDto.VoteType == VoteType.Downvote)
+        {
+            movieSuggestion.DownVoteCount++;
+        }
+
         await _dbContext.Votes.AddAsync(vote);
 
         // If vote was a veto, disqualify the suggestion
         if (createDto.VoteType == VoteType.Veto)
         {
-            movieSuggestionExists.IsDisqualified = true;
-        } else if (createDto.VoteType != VoteType.Veto && movieSuggestionExists.IsDisqualified)
+            movieSuggestion.IsDisqualified = true;
+        } 
+        else if (createDto.VoteType != VoteType.Veto && movieSuggestion.IsDisqualified)
         {
-            movieSuggestionExists.IsDisqualified = false;
+            movieSuggestion.IsDisqualified = false;
         }
 
+        await _hubContext.Clients
+            .GroupExcept(movieNightId, new[] {createDto.ConnectionId} )
+            .SendAsync("suggestion", movieNightId, ToMovieSuggestionDto(movieSuggestion ,parsedUserId), "vote");
         await _dbContext.SaveChangesAsync();
 
         return Ok(new { Message = "Vote added successfully" });
+    }
+
+    private static Object ToMovieSuggestionDto(MovieSuggestion ms, Guid initiatorId)
+    {
+         return new {
+            Id = ms.Id,
+            MovieId = ms.MovieId,
+            MovieNightEventId = ms.MovieNightEventId,
+            IsDisqualified = ms.IsDisqualified,
+            SuggestedBy = new
+            {
+                fullName = ms.SuggestedBy.FullName,
+                email = ms.SuggestedBy.Email
+            },
+            Movie = new TMDBMovieDto
+            {
+                TmdbId = ms.Movie.TmdbId,
+                Title = ms.Movie.Title,
+                PosterPath = ms.Movie.PosterPath,
+                ReleaseDate = ms.Movie.ReleaseDate.ToString(),
+                Overview = ms.Movie.Overview,
+                VoteAverage = ms.Movie.VoteAverage
+            },
+            ms.UpvoteCount,
+            ms.DownVoteCount,
+            UserVote = ms.Votes.Where(v => v.UserId == initiatorId).Select(v => v.VoteType).FirstOrDefault()
+        };
     }
 }
