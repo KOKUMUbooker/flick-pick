f8aed1b469d920085f37d0c9b2aecf5cb53b74e6
KOKUMUbooker <bookerochieng20@gmail.com>
2026-04-16 23:05:42 +0300

    Create movieNightEvent listener handler that calls the crud query cache functions based on the event actions

---
+++ UI/src/hubs/listeners/movie-event.svelte.ts
@@ -0,0 +1,21 @@
+import { AddMovieNightEventToQueryCache, DeleteMovieNightEventFromQueryCache, UpdateMovieNightEventToQueryCache } from "../../api/query-cache-crud";
+import { appState } from "../../store";
+import type { MovieNightEvent } from "../../types";
+
+export type MovieNightEventActions = "create" | "delete" | "edit"
+
+export function movieNightEventListeners() {
+    appState.hubConnection.off("movieEvent")
+
+    appState.hubConnection.on("movieEvent", async (movieEventId: string, movieEvent: MovieNightEvent, action: MovieNightEventActions, initiatorId?: string) => {
+        if (action === "create") {
+            await AddMovieNightEventToQueryCache(movieEventId, movieEvent)
+        }
+        else if (action === "delete") {
+            await DeleteMovieNightEventFromQueryCache(movieEventId, movieEvent.id, initiatorId)
+        }
+        else if (action === "edit") {
+            await UpdateMovieNightEventToQueryCache(movieEventId, movieEvent, initiatorId)
+        }
+    });
+}
