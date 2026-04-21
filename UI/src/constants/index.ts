16622fa1ed3ec9eeda6796f8c3fa75ce4f03ef10
KOKUMUbooker <bookerochieng20@gmail.com>
2026-04-21 21:11:55 +0300

    Add means to appropriately get user auth state if user is on public view & reloads page

--- UI/src/constants/index.ts
+++ UI/src/constants/index.ts
@@ -1,1 +1,2 @@
 export const CLIENT_ID = "watch-hive-web";
+export const ISLOGGED_KEY = "logged"
--- UI/src/routes/+layout.ts
+++ UI/src/routes/+layout.ts
@@ -1,1 +1,39 @@
+import { apiFetch, QUERY_KEYS, queryClient, type AuthResponseData } from '../api';
+import { API_BASE_URL } from '../api/urls';
+import { isLoggedIn, logIn, startHubConnection } from '../store';
+import type { LayoutLoad } from './$types';
+import { getLoggedStat } from "../utils/logged-status"
+
+// If user stat was previously 1 in lclStore, meaning user previously logged in,
+// refetch the user "loggedIn" status from server
+export const load: LayoutLoad = async () => {
+    const lclStoreLoggedStat = getLoggedStat();
+    if (lclStoreLoggedStat === "0" || isLoggedIn()) return;
+
+    try {
+        // fetchQuery returns the data directly
+        const userData = await queryClient.fetchQuery({
+            queryKey: [QUERY_KEYS.USER],
+            queryFn: async () => {
+                const res = await apiFetch<AuthResponseData>(
+                    `${API_BASE_URL}/api/auth/me`,
+                    {
+                        method: 'GET',
+                        headers: { 'Content-Type': 'application/json' },
+                    });
+                return res;
+            },
+            staleTime: 5 * 60 * 1000,
+            gcTime: 10 * 60 * 1000,
+        });
+
+        logIn(userData);
+
+        //  Connect to signalR hub
+        await startHubConnection();
+
+    } catch (err) {
+        console.error('Failed to fetch user:', err);
+    }
+};
 export const ssr = false;
